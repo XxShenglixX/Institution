e7b8bec754e0a6c00c6746e7a1e3bede9a386591
csl <c_shengli@hotmail.com>
2014-06-30 15:43:40 +0800

    Complete InstitutionReverse,isUniversityCollege incomplete InstitutionSelect

--- src/Institution.c
+++ src/Institution.c
@@ -5,13 +5,76 @@
 
 int Institution_reverse(LinkedList *inputList,LinkedList *outputList)
 {
+	int counter = 0;
+	Institution *input ,*output ;
 
 	Stack *newStack = Stack_create();
-	Institution *data = (Institution *)List_removeHead(inputList);
 	
-	Stack_push(newStack,(Institution *)data);
+	do
+	{
+		input = (Institution *)List_removeHead(inputList);
+		if(input!= NULL)	
+			{
+				Stack_push(newStack,input);
+				printf("input :%s\n",input->name);
+			}	
+	}while (input != NULL);
 	
-	//Stack_push(newStack,inputList->head);
+	// output = (Institution *)Stack_pop(newStack);
+	// printf("output :%s\n\n",output->name);
+	// List_addTail(outputList,output);
+	// output = (Institution *)Stack_pop(newStack);
+	// printf("output :%s\n\n",output->name);
+	// List_addTail(outputList,output);
 	
+	do
+	{
+		output = (Institution *)Stack_pop(newStack);
+		if(output !=NULL)
+			{
+				printf("output :%s\n",output->name);
+				List_addTail(outputList,output);
+				counter ++ ;
+			}
+	}while (output != NULL);
 	
+
+	
+	return counter ;
 }
+
+int isUniversityCollege(void *elem1,void *type)
+{
+	InstitutionType UCtype = *(InstitutionType *)type ;
+	printf("\ntype :%d\n",UCtype);
+
+	Institution *data = (Institution *)elem1 ;
+	printf("data UCtype :%d\n",data->type);
+	
+	if ( data->type == UCtype)
+		return 1 ;
+		
+	return 0;
+}
+
+int Institution_select(LinkedList *inputList,LinkedList *outputList,void *criterion,
+					   int (*compare)(void *,void *))
+{					   
+	Institution *head ,*output ;
+	int selected = 0 ,result;
+	
+	Stack *newStack = Stack_create();
+	
+	head = (Institution *)List_removeHead(inputList);
+	printf("\nInstituteSelect :%s\n",head->name);
+	
+	result = compare(head,criterion);
+	printf("Same ? : %d\n",result);
+	
+	if (result == 1)
+	{
+		
+	}
+	
+	return selected;
+}				
--- test/test_Institution.c
+++ test/test_Institution.c
@@ -4,23 +4,23 @@
 #include "mock_Stack.h"
 #include <stdio.h>
 
-LinkedList inList ;
+LinkedList inList;
 LinkedList outList;
 
 Stack stackInstance;
 
 void setUp(void)
 {
-	inList.head = NULL ;
-	inList.tail = NULL ;
 }
 
 void tearDown(void)
 {
 }
 
-void test_InstitutionReverse_explore()
+ void test_explore_InstitutionReverse_given_TARUC_should_return_1_for_reversed_list()
 {
+	int counter ;
+	
 	Institution TARUC = {"TARUC","KL",53300,12345678,UniversityCollege,1950};
 	
 	inList.head = &TARUC;
@@ -31,36 +31,75 @@ void test_InstitutionReverse_explore()
 	Stack_push_Expect(&stackInstance,&TARUC);
 	List_removeHead_ExpectAndReturn(&inList,NULL);
 	
-	//Stack_pop_ExpectAndReturn(&stackInstance,&TARUC);
-	//List_addTail(&outList,&TARUC);
+	Stack_pop_ExpectAndReturn(&stackInstance,&TARUC);
+	List_addTail_Expect(&outList,&TARUC);
+	Stack_pop_ExpectAndReturn(&stackInstance,NULL);
 	
-	Institution_reverse(&inList,&outList);
+	counter = Institution_reverse(&inList,&outList);	
+	printf("counter %d\n\n",counter);
 	
-	
-}
+	TEST_ASSERT_EQUAL(1,counter);
+} 
 
-/* void test_InstitutionReverse_explore()
+ void test_explore_InstitutionReverse_given_TARUC_UTAR_should_return_2_for_reversed_list()
 {
+	int counter ;
+	
 	Institution TARUC = {"TARUC","KL",53300,12345678,UniversityCollege,1950};
 	Institution UTAR = {"UTAR","KAMPAR",99999,87645612,University,2000};
 	
-	inList.head = &TARUC;
-	inList.tail = &UTAR;
-	
 	Stack_create_ExpectAndReturn(&stackInstance);
-	Stack_push_Expect(&stackInstance,&TARUC);
-	Stack_push_Expect(&stackInstance,&UTAR);
 	
-	List_create_ExpectAndReturn(&outList);
+	List_removeHead_ExpectAndReturn(&inList,&TARUC);
+	Stack_push_Expect(&stackInstance,&TARUC);
+	
+	List_removeHead_ExpectAndReturn(&inList,&UTAR);
+	Stack_push_Expect(&stackInstance,&UTAR);
+	List_removeHead_ExpectAndReturn(&inList,NULL);
 	
 	Stack_pop_ExpectAndReturn(&stackInstance,&UTAR);
-	List_addTail(&outList,&UTAR);
+	List_addTail_Expect(&outList,&UTAR);
 	
 	Stack_pop_ExpectAndReturn(&stackInstance,&TARUC);
-	List_addTail(&outList,&TARUC);
+	List_addTail_Expect(&outList,&TARUC);
+	Stack_pop_ExpectAndReturn(&stackInstance,NULL);
 	
-	Institution_reverse(&inList,&outList);
+	counter =Institution_reverse(&inList,&outList);	
 	
+	printf("counter %d",counter);
+	TEST_ASSERT_EQUAL(2,counter);
+}   
+
+void test_explore_isUniversityCollege_given_UniversityCollege_should_return_1()
+{
+	Institution TARUC = {"TARUC","KL",53300,12345678,UniversityCollege,1950};
+	InstitutionType UCtype = UniversityCollege ;
 	
+	TEST_ASSERT_EQUAL(1,isUniversityCollege(&TARUC,&UCtype));
 }
- */
+
+void test_explore_isUniversityCollege_given_University_should_return_0()
+{
+	Institution TARUC = {"TARUC","KL",53300,12345678,UniversityCollege,1950};
+	InstitutionType UCtype = University ;
+	
+	TEST_ASSERT_EQUAL(0,isUniversityCollege(&TARUC,&UCtype));
+}
+
+void test_explore_Institution_select_given_University_and_USM_UTAR_should_return_2()
+{
+	int selected ;
+	InstitutionType UCtype = University ;
+	Institution USM = {"USM","Penang",99999,87645612,University,2000};
+	Institution UTAR = {"UTAR","KAMPAR",99999,87645612,University,2000};
+	
+	Stack_create_ExpectAndReturn(&stackInstance);
+	
+	List_removeHead_ExpectAndReturn(&inList,&USM);
+	List_addTail
+	
+	
+	selected = Institution_select(&inList,&outList,&UCtype,isUniversityCollege);
+	TEST_ASSERT_EQUAL(2,selected);
+}
+ 
